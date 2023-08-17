67d2c82c387c9a7f40191f5d9cf2da1160a98345
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-17 22:07:53 +0530

    ,,

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const server = http.createServer(app); // Create HTTP server instance
 
 //middelwares
 app.use(cors({
-    origin: 'https://echochat.vercel.app'
+    origin: 'https://echoapp.vercel.app'
   }));
 // app.use(express.json());
 
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // Socket.IO configuration
 const io = new Server(server, {
     cors: {
-      origin: 'https://echochat.vercel.app',
+      origin: 'https://echoapp.vercel.app',
       methods: ['GET', 'POST'],
       allowedHeaders: ['my-custom-header'],
       credentials: true
